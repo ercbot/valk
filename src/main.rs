be96e79ba1fc8b0acf909e2cb5c7ae22cd12db98
Eric Botti <104158401+eric-botti@users.noreply.github.com>
2025-01-29 07:06:10 -0500

    Create Axum web server with screenshot endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,47 @@
+use axum::{
+    response::Json, routing::get, Router
+};
+
 use xcap::Monitor;
 use base64::{Engine as _, engine::general_purpose::STANDARD as BASE64};
 use image::ImageFormat;
+use serde_json::{json, Value}; 
 
-fn main() {
+async fn screenshot() -> Json<Value> {
     let monitors = Monitor::all().unwrap();
-    
-    for monitor in monitors {
-        let image = monitor.capture_image().unwrap();
-        
-        // Convert image to base64
-        let mut cursor = std::io::Cursor::new(Vec::new());
-        image.write_to(&mut cursor, ImageFormat::Png).unwrap();
-        let bytes = cursor.into_inner();
-        let base64_string = BASE64.encode(bytes);
 
-        // Print the first 20 characters of the base64 string
-        println!("{}", &base64_string[..20]);
- 
-    }
-    println!("I took a screenshot");
+    // Only get the first monitor
+    let monitor = monitors.first().unwrap();
+
+    let image = monitor.capture_image().unwrap();
+        
+    // Convert image to base64
+    let mut cursor = std::io::Cursor::new(Vec::new());
+    image.write_to(&mut cursor, ImageFormat::Png).unwrap();
+    let bytes = cursor.into_inner();
+    let base64_image = BASE64.encode(bytes);
+
+    // Create JSON object with "image" field
+    let response_json = Json(json!({
+        "image": base64_image
+    }));
+    
+    response_json
+}
+
+async fn root() -> &'static str {
+    "Subspace is running"
+}
+
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        // REST API
+        .route("/v1/screenshot", get(screenshot))
+        .route("/", get(root));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
